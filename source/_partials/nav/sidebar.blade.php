3e03a23c9e67794c9643e808a60ff6e5f43b3d3a
Sean Anderson <wing5wong@hotmail.com>
2019-08-01 16:17:08 +1200

    Update sidebar.blade.php

--- source/_partials/nav/sidebar.blade.php
+++ source/_partials/nav/sidebar.blade.php
@@ -2,7 +2,7 @@
     @php
     $col = $page->getCollection();
     @endphp
-    @foreach($$col->filter(function($p) use $page{
+    @foreach($$col->filter(function($p) use ($page) {
         return $p->getPath != $page->getPath();
     }) as $sub)
     <a href="{{$sub->getPath()}}" class="list-group-item list-group-item-action">{{$sub->title}}</a>
