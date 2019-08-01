c71186322d8c4fca33278173705f3d914ff1135c
Sean Anderson <wing5wong@hotmail.com>
2019-08-01 16:10:23 +1200

    Update 404.blade.php

--- source/404.blade.php
+++ source/404.blade.php
@@ -7,7 +7,24 @@ permalink: 404.html
 @section('title', 'Page not found')
 
 @section('content')
+<section class="content-section">
+    <div class="container" style="padding-top: 92px">
+        <div class="row">
+            <div class="col-sm-12">
+                
     <h1>404 Error</h1>
+    
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                
 
     <p>The page you were trying to access could not be found.</p>
+            </div>
+
+        </div>
+    </div>
+</section>
+
 @endsection
