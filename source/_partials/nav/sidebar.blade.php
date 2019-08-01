b98b6360125b71e62679f6110548e682fcd2080a
Sean Anderson <wing5wong@hotmail.com>
2019-08-01 16:16:29 +1200

    dont show same page here

--- source/_partials/nav/sidebar.blade.php
+++ source/_partials/nav/sidebar.blade.php
@@ -2,6 +2,8 @@
     @php
     $col = $page->getCollection();
     @endphp
-    @foreach($$col as $sub)
+    @foreach($$col->filter(function($p) use $page{
+        return $p->getPath != $page->getPath();
+    }) as $sub)
     <a href="{{$sub->getPath()}}" class="list-group-item list-group-item-action">{{$sub->title}}</a>
     @endforeach
