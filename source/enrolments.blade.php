49f5e4383d482beaeef4950d82331a0776170cd7
wing5wong <wing5wong@hotmail.com>
2019-08-09 01:06:49 +1200

    layout

--- source/enrolments.blade.php
+++ source/enrolments.blade.php
@@ -1,7 +1,14 @@
 @extends('_layouts.master')
 
 @section('content')
+<section class="content-section">
+    <div class="container" style="padding-top: 92px">
+        <div class="row">
+            <div class="col-sm-12">
 @foreach($enrolment as $e)
 <a href="{{$e->getPath()}}">{{$e->title}}</a>
 @endforeach
+            </div>
+        </div></div>
+</section>
 @endsection
--- source/our-school.blade.php
+++ source/our-school.blade.php
@@ -1,7 +1,12 @@
 @extends('_layouts.master')
 
 @section('content')
+<section class="content-section">
+    <div class="container" style="padding-top: 92px">
+        <div class="row">
+            <div class="col-sm-12">
 @foreach($our_school as $s)
 <a href="{{$s->getPath()}}">{{$s->title}}</a>
 @endforeach
+            </div></div></div></section>
 @endsection
