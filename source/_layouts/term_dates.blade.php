a6f29b526ea039698d8db5fe8f48c0ef0cfe5a76
wing5wong <wing5wong@hotmail.com>
2019-08-10 10:14:27 +1200

    term dates

---
+++ source/_layouts/term_dates.blade.php
@@ -0,0 +1,66 @@
+@extends('_layouts.master')
+
+@section('title', $page->title)
+
+@section('content')
+
+<section class="content-section">
+    <div class="container" style="padding-top: 92px">
+        <div class="row">
+            <div class="col-sm-12">
+                <h1>
+                    {{$page->title}}
+                </h1>
+            </div>
+        </div>
+        <div class="row">
+            @if ($page->image)
+            <img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover; height: 250px; width: 100%;">
+            @endif
+            <div class="col">
+                @yield('postContent')
+
+                @foreach($term_dates as $dates)
+                <h2>{{ $dates->title }}</h2>
+                <table>
+                    @foreach($dates->dates as $date)
+                    <tr>
+                        <td>
+                            {{ $date->title }}
+                        </td>
+                        <td>{{ date('F j, Y', $date["start_date"]) }}</td>
+                        <td>{{ date('F j, Y', $date["end_date"]) }}</td>
+                    </tr>
+                    @endforeach
+                </table>
+                @if(!$loop->last)
+                <hr>
+                @endif
+                @endforeach
+
+                @if($page->date)
+                <p>
+                    <strong>Updated {{ date('F j, Y', $page->date) }}</strong><br>
+                    <!-- @foreach ($page->tags as $tag)
+                    <a href="/tags/{{ $tag }}">{{ $tag }}</a>
+                    {{ $loop->last ? '' : '-' }}
+                    @endforeach -->
+                </p>
+                @endif
+
+                <blockquote data-phpdate="{{ $page->date }}">
+                    <em>WARNING: This post is over a year old. Some of the information this contains may be outdated.</em>
+                </blockquote>
+            </div>
+
+            <div class="col-sm-12 col-md-3">
+                @include('_partials.nav.sidebar')
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<hr>
+
+@endsection
