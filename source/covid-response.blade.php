3c2c96d6a55fbf4eb1cbebc85db87a7c23747cfb
Sean Anderson <wing5wong@hotmail.com>
2022-02-18 23:31:25 +1300

    Update covid-response.blade.php

--- source/covid-response.blade.php
+++ source/covid-response.blade.php
@@ -1,1 +1,25 @@
+@extends('_layouts.master')
 
+@section('content')
+<section class="content-section">
+    <div class="container" style="padding-top: 92px">
+        <div class="row">
+            <div class="col-sm-12">
+                <h1>
+                   COVID-19 Response
+                </h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="list-group list-group-flush">
+                @foreach($covid_response as $s)
+                <a href="{{$s->getPath()}}" class="list-group-item list-group-item-action">{{$s->title}}</a>
+                @endforeach
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
